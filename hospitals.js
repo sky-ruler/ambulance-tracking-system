bab70f64f6b445a6bc9253a5c644c8a6ee60400a
sky-ruler <skyruler3281@gmail.com>
2025-09-11 20:15:02 +0530

    Add files via upload

---
+++ hospitals.js
@@ -0,0 +1,83 @@
+// hospitals.js
+export const odishaHospitals = [
+  {
+    name: "AIIMS Bhubaneswar",
+    lat: 20.2961,
+    lng: 85.8245
+  },
+  {
+    name: "SCB Medical College, Cuttack",
+    lat: 20.4640,
+    lng: 85.8793
+  },
+  {
+    name: "KIMS, Bhubaneswar",
+    lat: 20.3540,
+    lng: 85.8170
+  },
+  {
+    name: "SUM Hospital, Bhubaneswar",
+    lat: 20.3080,
+    lng: 85.8190
+  },
+  {
+    name: "Hi-Tech Medical College, Bhubaneswar",
+    lat: 20.3400,
+    lng: 85.8120
+  },
+  {
+    name: "Capital Hospital, Bhubaneswar",
+    lat: 20.2762,
+    lng: 85.8333
+  },
+  {
+    name: "VIMSAR, Burla",
+    lat: 21.4969,
+    lng: 83.8726
+  },
+  {
+    name: "MKCG Medical College, Berhampur",
+    lat: 19.3115,
+    lng: 84.7929
+  },
+  {
+    name: "Balasore District Hospital",
+    lat: 21.4927,
+    lng: 86.9336
+  },
+  {
+    name: "Sundargarh District Hospital",
+    lat: 22.1167,
+    lng: 84.0333
+  },
+  {
+    name: "Keonjhar District Hospital",
+    lat: 21.6280,
+    lng: 85.5817
+  },
+  {
+    name: "Jagatsinghpur District Hospital",
+    lat: 20.2588,
+    lng: 86.1714
+  },
+  {
+    name: "Puri District Hospital",
+    lat: 19.8135,
+    lng: 85.8312
+  },
+  {
+    name: "Cuttack District Hospital",
+    lat: 20.4625,
+    lng: 85.8828
+  },
+  {
+    name: "Bolangir District Hospital",
+    lat: 20.7042,
+    lng: 83.4845
+  },
+  {
+    name: "Bhadrak District Hospital",
+    lat: 21.0585,
+    lng: 86.4958
+  }
+];
